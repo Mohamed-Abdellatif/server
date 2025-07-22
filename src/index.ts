bbdee6b520efc23ba8f1ca7e5b774c935ab79181
Mohamed-Abdellatif <mabdellatif411@gmail.com>
2025-07-22 22:01:11 +0300

    add cors and example .env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import cors from "cors";
+
 import userRoute from "./routes/userRoute";
 import announcementRoute from "./routes/announcementRoute";
 import quizRoute from "./routes/quizRoute"
@@ -10,6 +12,7 @@ dotenv.config();
 
 
 const app = express();
+app.use(cors());
 const port = 3001;
 
 app.use(express.json());
@@ -17,7 +20,6 @@ app.use(express.json());
 mongoose
   .connect(process.env.MONGO_DB_URL!)
   .then(() => console.log("Mongo Connected"));
-
 
 app.use('/user',userRoute)
 app.use('/announcement',announcementRoute);
